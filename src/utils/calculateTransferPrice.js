8a537560158db0f90740303300d85acddd21468b
Maximiliano Damian Maiz <maximaizrw@gmail.com>
2023-11-22 16:15:24 -0300

    Update calculateTransferPrice.js

--- src/utils/calculateTransferPrice.js
+++ src/utils/calculateTransferPrice.js
@@ -2,7 +2,7 @@ export function calculateTransferPrice(precioJuego, dolar) {
   
   var precioJuegoPesos = precioJuego * dolar;
   //Mi ganancia es el 15% del precio del juego
-  var ganancia = (precioJuegoPesos * 0.15);
+  var ganancia = (precioJuegoPesos * 0.10);
   console.log("ganancia: " + ganancia);
 
   var precioVenta = Math.round(precioJuegoPesos + ganancia);
